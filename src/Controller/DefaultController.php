856165a4b68b1563b607731f531568f9f8ac35f1
jules perret <perretjules@yahoo.fr>
2020-02-14 17:25:32 +0100

    page-accueil

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Repository\NewsRepository;
+use App\Repository\SchoolRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="child_index", methods={"GET"})
+     */
+    public function index(NewsRepository $newsRepository, SchoolRepository $schoolRepository): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'news' => $newsRepository->findAll(),
+            'school' => $schoolRepository->findOneBy([])
+        ]);
+    }
+}
